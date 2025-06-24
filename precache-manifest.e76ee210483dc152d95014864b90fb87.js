0c67b8a581e32e318933396044f03e62bba768a5
Mehdi Belhadi <mehdi.belhadi@icloud.com>
2025-06-24 16:42:52 +0200

    Updates

---
+++ precache-manifest.e76ee210483dc152d95014864b90fb87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12384de52555e0de6452876e8d2fbdd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65508d1e32b9344dcac9",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1a77954bf0a3fc372213",
+    "url": "/static/css/main.7581e0fa.chunk.css"
+  },
+  {
+    "revision": "65508d1e32b9344dcac9",
+    "url": "/static/js/2.b154b198.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b154b198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a77954bf0a3fc372213",
+    "url": "/static/js/main.c0fbbc83.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
